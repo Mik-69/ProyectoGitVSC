35e5c43d45d95c44968c0b5278ca33a0b533476f
Mik-69 <mikhailzatonsky@gmail.com>
2024-12-15 14:23:10 +0100

    Create Saludar.java

---
+++ Saludar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Saludor {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce tu nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.println("Hola " + nombre + " espero que tengas un muy buen dia");
+
+        System.out.print("Introduce tu nombre: ");
+        String nombre2 = sc.nextLine();
+
+        System.out.println("Adios " + nombre2 + " espero verte pronto");
+
+    }
+}
